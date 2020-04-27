1ad6fa10610191fb26b3ffacf40b1e69a52dc2b7
Tyler Konsonlas <tkonsonlas@hrgpros.com>
2020-04-27 13:08:32 -0700

    AD_Enabled_False - Notes

--- AD_Enabled_False.ps1
+++ AD_Enabled_False.ps1
@@ -1,1 +1,5 @@
 get-aduser -filter "(enabled -eq 'false')" -properties title, company, department,officephone, canonicalname, whencreated, officephone,office,mail,lastlogontimestamp | export-csv "C:\Users\tkonsonlas\Documents\AD2.csv"
+
+
+
+#Adding notes to this script
