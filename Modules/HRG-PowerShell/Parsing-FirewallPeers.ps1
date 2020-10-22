93f972239466968f49e9b6fed0281326122c2ede
Tyler Konsonlas <tkonsonlas@hrgpros.com>
2020-10-22 15:03:01 -0700

    setup-RestartVMs

--- Modules/HRG-PowerShell/Parsing-FirewallPeers.ps1
+++ Modules/HRG-PowerShell/Parsing-FirewallPeers.ps1
@@ -3,7 +3,7 @@
     #The curly brackets will also be needed around the variable
     #Pulled with 'Show vpn-sessiondb l2l', transfered to 'VPNsUp.xlsx'
 $template=@'
-Connection   : {ip*:199.91.236.20}
+{ip*:Connection   : 199.91.236.20}
 Index        : 63686                  IP Addr      : 199.91.236.20
 Protocol     : IKEv1 IPsecOverNatT
 Encryption   : IKEv1: (1)AES256  IPsecOverNatT: (1)AES256
@@ -12,7 +12,7 @@ Bytes Tx     : 257532170              Bytes Rx     : 153046045
 Login Time   : 03:57:56 PDT Fri Apr 24 2020
 Duration     : 7h:24m:57s
 
-Connection   : {ip*:216.54.236.201}
+{ip*:Connection   : 216.54.236.201}
 Index        : 2924                   IP Addr      : 216.54.236.201
 Protocol     : IKEv1 IPsec
 Encryption   : IKEv1: (1)AES256  IPsec: (2)3DES
@@ -23,7 +23,7 @@ Duration     : 13d 17h:54m:47s
 '@
 
 #explode file with template
-$listexploded=Get-Content -Path "C:\ScriptSource\peercheck.txt" | ConvertFrom-String -TemplateContent $template
+$listexploded=Get-Content -Path "C:\ScriptData\peercheck.txt" | ConvertFrom-String -TemplateContent $template
 
 #export csv 
-$listexploded | Export-Csv -Path "C:\ScriptOutput\peerOut.csv" -NoTypeInformation
+$listexploded | Export-Csv -Path "C:\ScriptData\peerOut.csv" -NoTypeInformation
---
+++ Modules/HRG-PowerShell/RestartVMs.ps1
@@ -0,0 +1,40 @@
+#Restart VM's that have been on for over X days
+$stat = 'sys.osuptime.latest'
+$now = Get-Date
+
+#Grab all machines powered 'on' and begin with 'HRGVM'
+$AllVMs = Get-VM | where{$_.PowerState -eq 'PoweredOn' -and $_.Name -like 'HRGVM*'}
+
+#Collect the uptime of all of the machines
+$AllVMuptime = Get-Stat -Entity $AllVMs -Stat $stat -Realtime -MaxSamples 1 |
+Select @{N='VM';E={$_.Entity.Name}},
+    #Column 'LastOSBoot' down to the second
+    @{N='LastOSBoot';E={$now.AddSeconds(- $_.Value)}},
+    #Column 'UptimeDays' round down to the nearest day
+    @{N='UptimeDays';E={[math]::Floor($_.Value/(24*60*60))}}
+
+#Filter the previous array down to just the VM's greater than X days
+$VMupXdays = $AllVMuptime |
+    Where {$_.UptimeDays -gt 60<#Number of days since last reboot#>} |
+    Export-Csv -Path "\\hrgatad\I\Helpdesk\VMRestartList.csv"
+
+#Restart each VM in the Xdays array
+ForEach ($VM in $VMupXdays)
+    {
+        get-vm $VM | Restart-VMGuest
+        #pausing for 60 seconds so that the server isn't overwhelmed
+        start-sleep -Seconds 60
+    }
+
+#Pause for 20 minutes before trying to run through the machines again
+Start-Sleep -Seconds 1200
+
+#After all of the initial restarts have been completed another pass
+#of the machines will be done to try powering up machines still 'off'
+ForEach ($VM in $VMupXdays)
+    {
+        if($VM.PowerState -eq "PoweredOff"){
+            Start-VM $VM
+            Start-Sleep -Seconds 60
+        }
+    }
