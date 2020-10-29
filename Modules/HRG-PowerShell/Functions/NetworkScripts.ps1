a75acd88900a0c9182420cfda6bf0e3865ccd52b
Tyler Konsonlas <tkonsonlas@hrgpros.com>
2020-10-29 06:18:28 -0700

    minor changes

--- Modules/HRG-PowerShell/Functions/NetworkScripts.ps1
+++ Modules/HRG-PowerShell/Functions/NetworkScripts.ps1
@@ -27,25 +27,64 @@ Write-Host -Object "`nOrder of checks:`nhttps - 443, http - 80, RDP - 3389, Prin
 $port_arr = @(443, 80, 3389, 9100)
 
   foreach ($port in $port_arr) {
-      Test-NetConnection $machine -port $port -InformationLevel Quiet
+    Test-NetConnection $machine -port $port -InformationLevel Quiet
   }
 }
 
 function Test-NetConPrimaryHIM {
+  $ip2check = Read-Host "IP to check"
   $count = 2
   $HIMaddress = "10.40.4."
-  #do {
+  $HIMaccess = $False
+  $success = $False
+  do {
       $iphost = $HIMaddress +$count
       if(Test-Connection $iphost -Quiet)
+      {
+        $success = $true
+        $PCname = Resolve-DnsName $iphost | select NameHost
+        Write-host "$PCname"
+        $HIMAccess = Invoke-Command -ComputerName $PCname.
         {
-          $success = $true
-          $PCname = Resolve-DnsName $iphost | select NameHost
-          Invoke-Command -ComputerName $PCname 
+          Test-Connection $HIMaccess -Quiet
         }
-      $count++
-  #} until ($count -eq 254 -or $success)
+      }
+      else { $count++ }
+  } until ($count -eq 254 -or $success)
   
-  
+  Write-host  "Result is: $HIMaccess"
 }
 
-$PCname = Test-Connection 10.40.4.2 | select Source
+
+<#
+
+Try{
+  $Array      = @()
+  $Servers    = Get-Content "c:\users\$env:username\desktop\servers.txt"
+  $PortNumber = '80'
+  $Array = Invoke-Command -cn $Servers {param($PortNumber)
+
+              $Destinations = "10.40.2.29","10.40.3.58"  
+                          
+              $Object = New-Object PSCustomObject
+              $Object | Add-Member -MemberType NoteProperty -Name "Servername" -Value $env:computername
+              $Object | Add-Member -MemberType NoteProperty -Name "Port" -Value $PortNumber
+
+              Foreach ($Item in $Destinations){
+                  $Result = Test-NetConnection -Port $PortNumber -cn $Item -InformationLevel Quiet
+                  $Object | Add-Member -Type NoteProperty -Name  "Destination" -Value $Item -Force
+                  $Object | Add-Member -Type NoteProperty -Name  "Connection" -Value $Result -Force
+                  $Object
+              } 
+                 
+                     
+  } -ArgumentList $PortNumber -ErrorAction stop | select * -ExcludeProperty runspaceid, pscomputername,PSShowComputerName
+}
+Catch [System.Exception]{
+  Write-host "Error" -backgroundcolor red -foregroundcolor yellow
+  $_.Exception.Message
+}
+
+$Array | Out-GridView -Title "Results" 
+$Array | Export-Csv $env:userprofile\desktop\results.csv -Force -NoTypeInformation 
+#>
