05f950f860374f8e245c62241dfd85cd68342d8e
Tyler Konsonlas <tkonsonlas@hrgpros.com>
2020-11-12 06:23:52 -0800

    changes

--- Modules/HRG-PowerShell/Functions/NetworkingTest.py
+++ Modules/HRG-PowerShell/Functions/NetworkingTest.py
@@ -3,6 +3,7 @@
 import textfsm
 from netmiko import ConnectHandler
 import getpass
+
 #Pulls the local username
 user = getpass.getuser()
 #Prompts for the password
@@ -10,17 +11,20 @@ pwd = getpass.getpass('Password:')
 #Store the device information
 ASA_VPN = {
     'device_type': 'cisco_ios',
-    'host': '192.168.205.3',
+    'host': '172.30.254.213',
     'username': user,
     'password': pwd,
     'secret' : pwd,
 }
+print ('\n### Connecting to the ASA ###\n')
 #setup the connection to the device within a session 'net_connect'
 net_connect = ConnectHandler(**ASA_VPN)
+
+net_connect.find_prompt()
 #elavate to enabled mode
-net_connect.enable()
+#net_connect.enable()
 #Send a command to the device, and store in 'result'
-result = net_connect.send_command("show vpn-sessiondb detail l2l")# ,use_textfsm=True
+result = net_connect.send_command('show access-list',use_textfsm=True)
 #Disconnect from the device
 net_connect.disconnect()
 
