2d53f4a4b0052df1151921243a1e3f4a76413b97
Tyler Konsonlas <tkonsonlas@hrgpros.com>
2020-12-07 08:25:19 -0800

    Formatting update

--- Modules/HRG-PowerShell/Functions/BasicFunctions.ps1
+++ Modules/HRG-PowerShell/Functions/BasicFunctions.ps1
@@ -1,19 +1,18 @@
 #Master list of all Basic PowerShell functions
 
-
 #Pull all users who are currently enabled
 function get-ADenabled {
-    get-aduser -filter "(enabled -eq 'true')" -properties title, company, department,officephone, canonicalname, whencreated, officephone,office,mail,lastlogontimestamp,employeeID | export-csv ".\ADenabled.csv" 
+    get-aduser -filter "(enabled -eq 'true')" -properties title, company, department, officephone, canonicalname, whencreated, officephone, office, mail, lastlogontimestamp, employeeID | export-csv ".\ADenabled.csv" 
 }
 
 #Pull all users who are currently disabled
 function get-ADdisabled {
-    get-aduser -filter "(enabled -eq 'false')" -properties title, company, department,officephone, canonicalname, whencreated, officephone,office,mail,lastlogontimestamp | export-csv ".\ADdisabled.csv"
+    get-aduser -filter "(enabled -eq 'false')" -properties title, company, department, officephone, canonicalname, whencreated, officephone, office, mail, lastlogontimestamp | export-csv ".\ADdisabled.csv"
 }
 
 #Pull all users who have a password that does not expire
 function get-ADPasswordNeverExpires {
-    get-aduser -Filter {(Enabled -eq $TRUE) -and (PasswordNeverExpires -eq $TRUE)} -ResultPageSize 2000 -Properties Name, SamAccountName, LastLogonDate, passwordlastset, distinguishedname | where-object {$_.distinguishedname -notlike "*disabled*" -and $_.distinguishedname -notlike "*roleaccounts*"} | export-csv ".\PassNeverExpiresAccounts.CSV"
+    get-aduser -Filter { (Enabled -eq $TRUE) -and (PasswordNeverExpires -eq $TRUE) } -ResultPageSize 2000 -Properties Name, SamAccountName, LastLogonDate, passwordlastset, distinguishedname | where-object { $_.distinguishedname -notlike "*disabled*" -and $_.distinguishedname -notlike "*roleaccounts*" } | export-csv ".\PassNeverExpiresAccounts.CSV"
 }
 
 <#
@@ -28,7 +27,7 @@ import-csv "C:\Users\tkonsonlas\OneDrive - Healthcare Resource Group, Inc\Docume
 #Pull all users who have not logged in for the last 60 days
 function get-ADnologgin60days {
     $60Days = (get-date).adddays(-60)
-    Get-ADUser -Filter {(Enabled -eq $TRUE) -and (PasswordNeverExpires -eq $TRUE)} -Properties Name,SamAccountName,LastLogonDate,DistinguishedName | Where {($_.LastLogonDate -le $60Days) -and ( $_.distinguishedname -notlike "*roleaccounts*") -and ( $_.distinguishedname -notlike "*sccm*")} | Sort | Select Name,SamAccountName,LastLogonDate,DistinguishedName | Export-Csv ".\PassNeverExpiresAccounts-Over60days.CSV" -NoTypeInformation
+    Get-ADUser -Filter { (Enabled -eq $TRUE) -and (PasswordNeverExpires -eq $TRUE) } -Properties Name, SamAccountName, LastLogonDate, DistinguishedName | Where { ($_.LastLogonDate -le $60Days) -and ( $_.distinguishedname -notlike "*roleaccounts*") -and ( $_.distinguishedname -notlike "*sccm*") } | Sort | Select Name, SamAccountName, LastLogonDate, DistinguishedName | Export-Csv ".\PassNeverExpiresAccounts-Over60days.CSV" -NoTypeInformation
 }
 
 <#
