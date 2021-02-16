308e3c4d614562f6dfb03b287d9043e1c3637f18
Tyler Konsonlas <tkonsonlas26@outlook.com>
2021-02-16 11:56:08 -0800

    x

--- Modules/SysAdmin_Utility/Functions/aabMainMenu.ps1
+++ Modules/SysAdmin_Utility/Functions/aabMainMenu.ps1
@@ -24,7 +24,7 @@ function Get-MainMenu {
     do{
         #The options are shown
         Show-OptionMenu
-        $input = Read-Host "`nEnter the number of your desired action: "
+        $input = Read-Host "`nEnter the number of your desired action('q' to quit)"
         switch ($input){#Functions that will be used based on the users selection
         '1' {   
             Write-Host "    Pulling the enabled users from AD" -Color Green
