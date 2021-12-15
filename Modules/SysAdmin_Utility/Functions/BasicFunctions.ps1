5fdbf919834f5df8014175e83204a2c0e12612c7
Tyler Konsonlas <tkonsonlas26@outlook.com>
2021-12-15 10:45:39 -0800

    SelfpayDB update

--- Modules/SysAdmin_Utility/Functions/BasicFunctions.ps1
+++ Modules/SysAdmin_Utility/Functions/BasicFunctions.ps1
@@ -195,14 +195,18 @@ function Get-MembersOfGroups {
 Test-Connection -Source 10.40.4.140 -ComputerName 192.168.1.52
 Test-Connection -Source 10.40.0.234 -ComputerName 192.168.1.53
 
-
-
 function -SelfpayDB {
 Enter-PSSession -ComputerName vhrgihpe
 
 Get-SmbOpenFile | Where-Object {$_.Path -like "Q:\EMPLOYEE\SPAYDataBase\Supervisor Interface*"} | Close-SmbOpenFile -Force
 Start-Sleep -Seconds 1
+
+Remove-Item -Path "Q:\EMPLOYEE\SPAYDataBase\Supervisor Interface.accdb"
+Remove-Item -Path "Q:\EMPLOYEE\SPAYDataBase\Supervisor Interface.laccdb"
+Start-Sleep -Seconds 1
+
 Copy-Item "Q:\EMPLOYEE\SPAYDataBase\back up-dont't touch please\Supervisor Interface-Current.accdb" "Q:\EMPLOYEE\SPAYDataBase\Supervisor Interface-Current.accdb"
+Start-Sleep -Milliseconds 300
 Rename-Item "Q:\EMPLOYEE\SPAYDataBase\Supervisor Interface-Current.accdb" "Supervisor Interface-testing.accdb"
 
 Exit-PSSession
