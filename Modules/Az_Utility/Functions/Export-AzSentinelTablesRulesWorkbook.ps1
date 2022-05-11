e342a11c32453283a93708c1d3da6ce3206811bf
Tyler Konsonlas <tkonsonlas26@outlook.com>
2022-05-10 20:22:28 -0700

    LogSource-updated

--- Modules/Az_Utility/Functions/Export-AzSentinelTablesRulesWorkbook.ps1
+++ Modules/Az_Utility/Functions/Export-AzSentinelTablesRulesWorkbook.ps1
@@ -40,10 +40,10 @@
 Function Export-AzSentinelAnalyticsRules {
     param (
         #flip to true, comment out default 
-        [Parameter(Mandatory = $false)]  [string]$WorkSpaceName = "CISOLogAnalyticsWorkspace", #"3PSIEM",
+        [Parameter(Mandatory = $false)]  [string]$WorkSpaceName = "CISOLogAnalyticsWorkspace",#"3PSIEM",
         #flip to true, comment out default
         [Parameter(Mandatory = $false)]  [string]$ResourceGroupName = "cisosentinelloganalytics_rg",#"danh2",
-        [Parameter(Mandatory = $false)] [string]$FileName = "Sentinel_CurrentRules_testing.csv" #default
+        [Parameter(Mandatory = $false)] [string]$FileName = "Sentinel_CurrentRules_Client.csv" #default
     )
 
     if (! $Filename.EndsWith(".csv")) { $FileName += ".csv"}
@@ -60,26 +60,26 @@ Function Export-AzSentinelAnalyticsRules {
     
     $SubscriptionId = (Get-AzContext).Subscription.Id
 
-    #First API call to grab all of the existing tables for the client
-    $url = "https://management.azure.com/subscriptions/$($subscriptionId)/resourceGroups/$($resourceGroupName)/providers/Microsoft.OperationalInsights/workspaces/$($workspaceName)/providers/Microsoft.SecurityInsights/metadata?api-version=2017-01-01-preview"
+    #Second API call for all of the Table Rules
+    #Load the templates so that we can copy the information as needed
+    $url = "https://management.azure.com/subscriptions/$($subscriptionId)/resourceGroups/$($resourceGroupName)/providers/Microsoft.OperationalInsights/workspaces/$($workspaceName)/providers/Microsoft.SecurityInsights/alertRules?api-version=2021-10-01"
+    #Calling a JSON file with the 'Invoke-RestMethod' so that Powershell can work with the data
     $results = (Invoke-RestMethod -Method "Get" -Uri $url -Headers $authHeader ).value
 
     #Create the list variable for all of the tables
-    $tables = New-Object System.Collections.Generic.List[string]
-    foreach ($table in $results) {
-        #Grab the tablenames
-		$tableName = $result.name
-        #Add the table name to the list
-        $tables.add($tableName)
-    }
-    #Adding custom table/function from the client
-    $tables.Add('Sysmon')
-
-    #Second API call for all of the Table Rules
-    #Load the templates so that we can copy the information as needed
-    $url = "https://management.azure.com/subscriptions/$($subscriptionId)/resourceGroups/$($resourceGroupName)/providers/Microsoft.OperationalInsights/workspaces/$($workspaceName)/providers/Microsoft.SecurityInsights/alertRules?api-version=2019-01-01-preview"
-    #Calling a JSON file with the 'Invoke-RestMethod' so that Powershell can work with the data
-    $results = (Invoke-RestMethod -Method "Get" -Uri $url -Headers $authHeader ).value
+    $tables = @('SecurityEvent','CommonSecurityLog','InsightsMetrics','Event','DeviceEvents',
+    'AADNonInteractiveUserSignInLogs','DeviceProcessEvents','DeviceRegistryEvents','BehaviorAnalytics',
+    'W3CIISLog','DeviceNetworkEvents','DnsEvents','DeviceFileEvents','AWSCloudTrail','Heartbeat',
+    'DeviceImageLoadEvents','Syslog','DeviceFileCertificateInfo','Perf','OfficeActivity','SigninLogs',
+    'DeviceNetworkInfo','AuditLogs','DeviceLogonEvents','ThreatIntelligenceIndicator','DeviceInfo',
+    'EmailEvents','CloudAppEvents','AADProvisioningLogs','DnsInventory','EmailUrlInfo','EmailAttachmentInfo',
+    'UserPeerAnalytics','AADServicePrincipalSignInLogs','SalesforceServiceCloud_CL','ComputerGroup',
+    'Operation','AzureActivity','Update','SailPointIdentityIQ_CL','OfficeAudit_CL','Dynamics365Activity',
+    'IdentityInfo','SecurityRecommendation','SecurityAlert','SecurityIncident','Anomalies','MDEAudit_CL',
+    'SecurityBaseline','TeamsAuditLogs_CL','SecurityNestedRecommendation','IntuneOperationalLogs',
+    'InformationProtectionLogs_CL','Watchlist','IntuneAuditLogs','ProtectionStatus','UserAccessAnalytics',
+    'AADManagedIdentitySignInLogs','IdentityIQ_CL','EmailPostDeliveryEvents','AzureMetrics','AzureDiagnostics',
+    'UpdateSummary','SecurityBaselineSummary','IncidentFileActions_CL','IncidentProcessActions_CL','Sysmon')
 
     foreach ($result in $results) {
         #Escape the description field so it does not cause any issues with the CSV file
@@ -89,40 +89,82 @@ Function Export-AzSentinelAnalyticsRules {
 
         #Create and output the line of information.
 		$displayName = $result.properties.displayName
+        $alertID = $result.name
         $enabledrule = $result.properties.enabled
 		$severity = $result.properties.severity
         $kind = $result.kind
         $description = $result.properties.description
         $query = $result.properties.query
-
+        $entities = $result.properties.entityMappings
+        
         #Make sure the log source is first empty
         $logSource = ""
+        <# - Old table code
         #Verify the query is not empty
-        if( $query -ne ""){
+        if($query.length -gt 3){
             #Iterate through the tables, and search the query for which table is used
-            foreach($table2 in $tables) {
-                if($query.Indexof($table2)) {
+            foreach($table in $tables) {
+                if($query.Contains($table)) {
                     #Add any tables that were found within the Query
-                    $logSource += $table2 + "|"
+                    $logSource += $table + "|"
+                }
+            }
+        }#>
+        #New table code
+        if($query.length -gt 3){
+            #Iterate through the tables, and search the query for which table is used
+            foreach($table in $tables) {
+                if($table -eq 'Event' -or 
+                    $table -eq 'Operation' -or
+                    $table -eq 'Update' -or
+                    $table -eq 'Perf'
+                    ) {
+                    $pattern = ""
+                    $pattern = '^\s*' + [regex]::Escape($table) + '[\s|\n]+|\s+' + [regex]::Escape($table) + '[\s|\n]+'
+                    if($query -match $pattern) {
+                        $logSource += $table + "|"
+                    }
+                }
+                elseif($query.Contains($table)) {
+                    #Add any tables that were found within the Query
+                    $logSource += $table + "|"
                 }
             }
         }
+
         #If we have an entry, remove the last pipe character
-        if ("" -ne $logSource) {
+        if ($logSource.length -gt 3) {
             $logSource = $logSource.Substring(0, $logSource.length - 1)
+        }
+
+        $entitylist = ""
+
+        if ($entities.length -gt 0) {
+            foreach($entity in $entities) {
+                $entitylist += $entity.entityType + ":"
+                $entitylist += $entity.fieldMappings.identifier + ","
+                $entitylist += $entity.fieldMappings.columnname + "|"
+            }
+        }
+
+        #If we have an entry, remove the last pipe character
+        if ($entitylist.length -gt 3) {
+            $entitylist = $entitylist.Substring(0, $entitylist.length - 1)
         }
 
 		[pscustomobject]@{ 
             'DisplayName' = $displayName;
+            'AlertID' = $alertID;
             'Enabled' = $enabledrule;
             'Severity' = $severity;
             'Kind' = $kind;
             'Description' = $description;
             'LogSource' = $logSource;
-            'Entities' = "";
+            'Entities' = $entitylist;
             'Query' = $query
         } | Export-Csv $filename -Append -NoTypeInformation
     }
+    
 }
 
 #Comment out if you want to just use the Az_Utility Module to call this function
